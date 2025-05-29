d93b71223459d6e6b5070c3ade4fd4a1e1784616
jeffgodd <jeff@leggierosw.com>
2025-05-29 15:13:42 -0700

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Jeff Goddard on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
